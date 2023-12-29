7d1710de70237f5a2aa99924ffc81be154682089
Huot Rithy <110338150+Liqaing@users.noreply.github.com>
2023-12-29 21:17:06 +0700

    Create ribbon

---
+++ BMToolkits/BMToolkits/Ribbon1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BMToolkits
+{
+    public partial class ribbon
+    {
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+            //Excel.Worksheet activeSheet = (Excel.Worksheet) Excel.Application.ActiveSheet;
+
+        }
+
+
+        // Sumarry Invoice From Other Files
+        private void otherFiles_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+        // Summary Invoice From Same Files
+        private void sameFiles_Click(object sender, RibbonControlEventArgs e)
+        {
+            sameFileControl formControl = new sameFileControl();
+            formControl.Show();
+            
+        }
+    }
+}
---
+++ BMToolkits/BMToolkits/sameFileControl.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BMToolkits
+{
+    partial class sameFileControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.filterOption = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sheetName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter Options";
+            // 
+            // filterOption
+            // 
+            this.filterOption.AllowDrop = true;
+            this.filterOption.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filterOption.FormattingEnabled = true;
+            this.filterOption.Items.AddRange(new object[] {
+            "Contain",
+            "Equal to"});
+            this.filterOption.Location = new System.Drawing.Point(22, 101);
+            this.filterOption.Name = "filterOption";
+            this.filterOption.Size = new System.Drawing.Size(167, 28);
+            this.filterOption.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Sheets";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(225, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sheet Name";
+            // 
+            // sheetName
+            // 
+            this.sheetName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sheetName.Location = new System.Drawing.Point(229, 101);
+            this.sheetName.Name = "sheetName";
+            this.sheetName.Size = new System.Drawing.Size(178, 26);
+            this.sheetName.TabIndex = 4;
+            // 
+            // sameFileControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.sheetName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filterOption);
+            this.Controls.Add(this.label1);
+            this.Name = "sameFileControl";
+            this.Size = new System.Drawing.Size(830, 208);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox filterOption;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sheetName;
+    }
+}
